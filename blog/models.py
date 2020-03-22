c0fab0b2a9c1e33af5a28a20730aaa59f9d808e7
akshatkarodiya <akshatkarodiya@gmail.com>
2020-03-23 01:04:56 +0530

    Added Bootstrap and job frontend

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField('images/')
+
+
+
+# title
+#pub_date
+# body
+# images
+
+# add the blog app to the setting
+# create a migration
+# migrate
+# add to the the admin
